762c008542aabb4b3f8ebb23fd17ad704e1854e8
Kunal Palawat <palawat.kunal@gmail.com>
2023-10-19 13:02:00 -0700

    exceedance script

---
+++ scripts_active/exceedances.R
@@ -0,0 +1,166 @@
+#
+##
+###
+###
+###
+###
+###
+###
+###
+
+#load libraries ----
+library(readxl)
+library(tidyverse)
+library(aod)
+
+#load data ----
+standards <- read_xlsx("/Users/gift/Documents/GitHub/WorkingFiles/data/data_processing/Standards.xlsx", sheet = "standards", col_names = TRUE)
+#standards <- read_xlsx("~/Documents/GitHub/ProjectHarvest/WorkingFiles/data/data_processing/Standards.xlsx", sheet = "standards", col_names = TRUE)
+
+#join standard data to contaminant data, this repeats the standard data for each analyte for each sample
+ex.dat <- full_join(iw.dm.long, standards, by = c("analyte"))
+
+#make longer
+ex.dat.long <- pivot_longer(data = ex.dat,
+                            cols = PB:LDW,
+                            values_to = "standard_value",
+                            names_to = "standard")
+#calculate exceedance
+ex.dat.long$exceedance <- ifelse(ex.dat.long$value > ex.dat.long$standard_value,1,0)
+
+#remove standard value for ease of making wider
+ex.dat.long <- subset(ex.dat.long, select = -c(standard_value))
+
+#remove NAs for analytes that do not have standards
+ex.dat.long <- ex.dat.long %>%
+  drop_na(exceedance)
+
+#summary ----
+#name subset columns
+cols <- c("standard", "analyte")
+
+#calculate counts and percentages of the whole
+sumtable <- ex.dat.long %>%
+  group_by(across(all_of(cols))) %>%
+  summarise(n = n(),
+            exceedances_n = sum(exceedance),
+            exceedances_freq = signif(sum(exceedance)/n()*100, 2))
+
+sumtable.small <- subset(sumtable, select = -c(n, exceedances_n))
+
+sumtable.wide <- pivot_wider(data = sumtable,
+                             names_from = "standard",
+                             values_from = "exceedances_freq")
+view(sumtable.wide)
+write.csv(sumtable.wide, "")
+
+#modeling ----
+ex.dat.long
+
+e1 <- glm(data = ex.dat.long[ex.dat.long$standard=="AI"&ex.dat.long$analyte=="Cu",],
+          exceedance ~ season + community,
+          family = "binomial")
+summary(e1)
+check_model(e1)
+exp(coef(e1))
+performance(e1)
+
+e2 <- glm(data = ex.dat.long[ex.dat.long$standard=="AI"&ex.dat.long$analyte=="Cu",],
+          exceedance ~ season,
+          family = "binomial")
+summary(e2)
+check_model(e2)
+exp(coef(e2))
+performance(e2)
+
+anova(e1, e2)
+
+exp(-4.7431 + .8778)/(1+(exp(-4.7431 + .8778)))
+exp(-4.7431)/(1+(exp(-4.7431)))
+
+                                     
+#odds of having an AI exceedance during winter is 
+
+#Scratch Work ----
+summarize(n = n(),
+            min = min(.data[[exceedance]]),
+            max = max(.data[[exceedance]]),
+            median = median(.data[[exceedance]]),
+            mean = mean(.data[[exceedance]]),
+            sd = sd(.data[[exceedance]])
+            ,
+            gmean = geoMean(.data[[exceedance]]),
+            gsd = geoSD(.data[[exceedance]])
+  )
+
+
+sumFX <- function(datalongDF, subset.vector.string, value.string, dfname.string, filename.string){
+  
+  #load libraries
+  library(tidyverse)
+  library(EnvStats)
+  
+  #assign data
+  dat.long <- datalongDF
+  cols <- subset.vector.string
+  value <- value.string
+  dfname <- dfname.string
+  filename <- filename.string
+  
+  #calculate summary stats
+  sumtable <- dat.long %>%
+    group_by(across(all_of(cols))) %>%
+    summarize(n = n(),
+              min = min(.data[[value]]),
+              max = max(.data[[value]]),
+              median = median(.data[[value]]),
+              mean = mean(.data[[value]]),
+              sd = sd(.data[[value]])
+              ,
+              gmean = geoMean(.data[[value]]),
+              gsd = geoSD(.data[[value]])
+    )
+  
+  #make longer
+  sum.long <- pivot_longer(data = sumtable,
+                           cols = n:gsd,
+                           values_to = "value",
+                           names_to = "stat",
+                           values_drop_na = T)
+  
+  #sig figs
+  sum.long$value <- formatC(signif(as.numeric(sum.long$value),digits=3), digits=3,format="fg", flag="#")
+  
+  sum.long$value <- as.numeric(sum.long$value)
+  
+  #widen
+  sum.wide <- pivot_wider(data = sum.long,
+                          names_from = stat, #change out as needed
+                          values_from = value)
+  
+  #sig figs messes up count for some reason, add from original
+  sum.wide$n <- sumtable$n
+  
+  #save as csv file in your working directory
+  write.csv(sum.wide, paste(filename,"_sum.csv", sep = ""))
+  
+  #copy to new dataframe with a unique name and place in global environment
+  assign(paste(dfname), sum.wide, envir=.GlobalEnv)
+  
+  # #return the dataframe by character string
+  # return(get(dfname))
+  
+}
+
+
+
+ex.dat$`PBex` <- ifelse(ex.dat$value > ex.dat$PB, 1,0)
+ex.dat$`FBex` <- ifelse(ex.dat$value > ex.dat$FB, 1,0)
+ex.dat$`DWex`  <- ifelse(ex.dat$value > ex.dat$DW, 1,0)
+ex.dat$`AIex`  <- ifelse(ex.dat$value > ex.dat$AI, 1,0)
+ex.dat$`LDWex`  <- ifelse(ex.dat$value > ex.dat$LDW, 1,0)
+
+ex.dat.long <- pivot_longer(data = ex.dat,
+                            cols = PBex:LDWex,
+                            names_to = "standard",
+                            values_to = "exceedance")
