4deb1ac7f54c401136d9ddc437492d307f077848
Kunal Palawat <palawat.kunal@gmail.com>
2023-11-02 12:35:22 -0700

    Update PCA.R

--- scripts_active/PCA.R
+++ scripts_active/PCA.R
@@ -7,6 +7,27 @@
 
 
 #load libraries
+library(elasticnet)
+library(scPCA)
+library(sparsepca)
 
+#correlation ----
+# Plot the correlation matrix of the generated data, natural log transformed
+data <- log(iw.dm[,9:27])
+cor_matrix <- cor(data)
+# Convert the correlation matrix to a long format
+cor_matrix_long <- reshape2::melt(cor_matrix)
+# Create the heatmap plot using ggplot2
+heatmap_plot <- ggplot(cor_matrix_long, aes(Var1, Var2, fill = value)) +
+  geom_tile() +
+  scale_fill_gradient(low = "#02d7f2", high = "#E93CAC", name = "Correlation") +
+  labs(title = "Correlation Matrix of Generated Data")
+# Print the heatmap plot
+print(heatmap_plot)
 
-#load data
+#sparce PCA with elasticnet ----
+penalties <- exp(seq(log(10), log(1000), length.out = 6))
+spca_sim_p <- elasticnet::spca(data, K = 6, para = rep(penalties, 20),
+                               type = "predictor", sparse = "penalty")
+print(spca_sim_p$pev)
+print(spca_sim_p$loadings)
