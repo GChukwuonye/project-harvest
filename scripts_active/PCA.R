8b50e1118ac3529ea56e19d2fe232f266d15e304
Kunal Palawat <palawat.kunal@gmail.com>
2023-11-02 11:40:21 -0700

    PCA

---
+++ scripts_active/PCA.R
@@ -0,0 +1,12 @@
+#Kunal Palawat Gift Chukwuonye
+#Date:
+#Description: Space PCA analysis to narrow down which analytes to focus on for modeling
+#
+#NOTES  
+  # looking at detection summaries, all analytes are generally above 30% detection except for Be and Se in Y3, but in Y1 and Y2, they are above 30%, so these analytes will be included in analysis
+
+
+#load libraries
+
+
+#load data
