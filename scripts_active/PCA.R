580da2bac9b1041d9268141557e7e81f56b1f135
Kunal Palawat <palawat.kunal@gmail.com>
2023-11-26 17:25:42 -0800

    hds summaries and modeling

--- scripts_active/PCA.R
+++ scripts_active/PCA.R
@@ -12,6 +12,18 @@ library(elasticnet)
 library(scPCA)
 library(sparsepca)
 
+# Load necessary libraries
+library(sparsepca)
+library(MASS)  # For generating multivariate normal data
+library(caret)  # For cross-validation
+library(ggplot2)
+library(glmnet)
+library(elasticnet)  # Load the elasticnet package for sparse PCA
+library(matrixcalc)  # Load necessary library for nearestPD function
+library(Matrix)
+library(scPCA)
+library(dplyr)
+
 #load data----
 #remove samples with NAs for pH (54 samples)
 dat <- iw.ln.dm %>%
@@ -32,40 +44,198 @@ datgm <- datgm[,c("pH","Al", "Sb", "As", "Ba", "Be", "Cd", "Cr", "Co", "Cu", "Fe
 dathw <- dathw[,c("pH","Al", "Sb", "As", "Ba", "Be", "Cd", "Cr", "Co", "Cu", "Fe", "Pb", "Mn", "Mo", "Ni", "Se", "Ag", "Sn", "V", "Zn")]
 dattu <- dattu[,c("pH","Al", "Sb", "As", "Ba", "Be", "Cd", "Cr", "Co", "Cu", "Fe", "Pb", "Mn", "Mo", "Ni", "Se", "Ag", "Sn", "V", "Zn")]
 
+datpca <- dat[,c("pH","Al", "Sb", "As", "Ba", "Be", "Cd", "Cr", "Co", "Cu", "Fe", "Pb", "Mn", "Mo", "Ni", "Se", "Ag", "Sn", "V", "Zn")]
+
+
+#PCA
+pca_sim <- prcomp(datpca)
+pca_sim <- prcomp(datdh)
+pca_sim <- prcomp(datgm)
+pca_sim <- prcomp(dathw)
+pca_sim <- prcomp(dattu)
+
+# plot the 2D rep using first 2 components
+df <- data.frame(list("PC1" = pca_sim$x[, 1],
+                      "PC2" = pca_sim$x[, 2]))
+p_pca <- ggplot(df, aes(x = PC1, y = PC2)) +
+  ggtitle("PCA on All Communities Data") +
+  geom_point(alpha = 0.5) +
+  theme_minimal()
+p_pca
+
+# Extract eigenvalues
+eigenvalues <- (pca_sim$sdev)^2
+
+# Calculate the percentage of variance explained
+variance_explained <- eigenvalues / sum(eigenvalues) * 100
+
+# Create a data frame for the scree plot
+scree_data <- data.frame(PC = 1:length(variance_explained), VarianceExplained = variance_explained)
+
+# Create the scree plot as a scatterplot with a connecting line
+scree_plot <- ggplot(scree_data, aes(x = PC, y = VarianceExplained)) +
+  geom_point(color = "steelblue", size = 3) +
+  geom_line(color = "red") +
+  labs(title = "Scree Plot for PCA",
+       x = "Principal Component",
+       y = "Percentage of Variance Explained") +
+  theme_minimal()
+
+# Display the scree plot
+print(scree_plot)
+
+# 
+# We may use the skree plot above to choose the number of principal components. We look for the "elbow" in the plot where diminishing returns from including an additional principal component begin to appear.
+# All - 3 PCs
+# DH - 4 PCs
+# GM - 2 Pcs
+# HW - 4 PCs
+# TU - 4 PCs
+# 
+# Elbow happens 2 - 4 PCs, tune sparce PCA with 4 components.
+# 
+# The parameter we are tuning, alpha, controls an L1 penalty term. We will tune the sparse PCA using the Index of Sparseness or IS. The index of sparseness is defined as
+# $$IS = \mathrm{PEV}_{pca}\times \mathrm{PEV}_{sparse}\times \mathrm{PS}$$
+#   Where $\mathrm{PEV}_{pca}, \mathrm{PEV}_{sparse}$, and $\mathrm{PS}$ are the Percentage of Explained Variance (PEV) from the selected principal components of the standard PCA, the PEV from the selected principal components of the sparse PCA, and the proportion of sparsity (number of entries equal to zero in the component loadings) respectively.
+# 
+# Tuning penalty term $\alpha$ using index of sparseness:
+pca_result <- prcomp(datpca)
+
+# Calculate PEV_pca
+PEV_pca <- sum(pca_result$sdev[1:3]^2) / sum(pca_result$sdev^2)
+
+# Set up a grid of alpha values to search
+alpha_grid <- seq(0.05, 1, by = 0.05)
+
+# Create an empty list to store the results
+results <- list()
+
+# Perform cross-validation to tune alpha
+#k=4 because we chose to use 4 prinicipal components
+for (alpha in alpha_grid) {
+  spca_model <- elasticnet::spca(datpca, K = 4, para = rep(alpha, 4), type = "predictor", sparse = "penalty")
+  results[[as.character(alpha)]] <- spca_model
+}
+# Calculate PEV_sparse for each alpha
+for (alpha in alpha_grid) {
+  spca_model <- results[[as.character(alpha)]]
+  PEV_sparse <- sum(spca_model$pev)
+  results[[as.character(alpha)]]$PEV_sparse <- PEV_sparse
+}
+# Calculate the proportion of sparsity for each alpha
+for (alpha in alpha_grid) {
+  spca_model <- results[[as.character(alpha)]]
+  proportion_of_sparsity <- sum(spca_model$loadings == 0) / length(spca_model$loadings)
+  results[[as.character(alpha)]]$proportion_of_sparsity <- proportion_of_sparsity
+  # Calculate IS
+  results[[as.character(alpha)]]$IS <- PEV_pca * PEV_sparse * proportion_of_sparsity
+}
+# Find the alpha with the maximum IS
+optimal_alpha <- alpha_grid[which.max(sapply(results, function(result) result$IS))]
+
+# Get the sparse PCA model with the optimal alpha
+optimal_spca_model <- results[[as.character(optimal_alpha)]]
+# Extract IS values and alpha values
+IS_values <- sapply(results, function(result) result$IS)
+alpha_values <- as.numeric(names(results))
+
+# Plot IS versus alpha
+plot(alpha_values, IS_values, type = "l", xlab = "Alpha (Sparsity Parameter)", ylab = "Index of Sparseness (IS)", 
+     main = "IS vs. Alpha")
+
+# Add a point for the optimal alpha
+points(optimal_alpha, results[[as.character(optimal_alpha)]][["IS"]], col = "red", pch = 19)
+legend("topleft", legend = c("Optimal Alpha"), col = c("red"), pch = c(19))
+
+
+# From the plot above, we can see that alpha values from 0.60 to roughly 1.0 maximize the Index of Sparseness value. Opting for the lowest optimal penalty value, the loadings for the Optimal Sparse PCA with $\alpha =0.6$ are shown below:
+#   
+optimal_spca_model$loadings
+
+
 #sparce PCA with elasticnet ----
-#k = 6 means only the first 6 principal componants are shown
+#k = 4 means only the first 6 principal componants are shown
 #adding the penalty helps to zero out some of the variables and prevent over fitting (this essentially does variable selection for us!)
-penalties <- exp(seq(log(10), log(1000), length.out = 6))
+penalties <- exp(seq(log(10), log(1000), length.out = 4))
 
 ##dh----
-spca_sim_p <- elasticnet::spca(x=datdh, K = 6, para = rep(penalties, 20),
+spca_sim_p <- elasticnet::spca(x=datdh, K = 4, para = rep(penalties, 20),
                                type = "predictor", sparse = "penalty")
 print(spca_sim_p$pev)
 print(spca_sim_p$loadings)
 #write.csv(spca_sim_p$loadings, "dh_elasticloadings.csv")
 
 ##gm ----
-spca_sim_p <- elasticnet::spca(x=datgm, K = 6, para = rep(penalties, 20),
+spca_sim_p <- elasticnet::spca(x=datgm, K = 4, para = rep(penalties, 20),
                                type = "predictor", sparse = "penalty")
 print(spca_sim_p$pev)
 print(spca_sim_p$loadings)
 write.csv(spca_sim_p$loadings, "gm_elasticloadings.csv")
 
 ##hw ----
-spca_sim_p <- elasticnet::spca(x=dathw, K = 6, para = rep(penalties, 20),
+spca_sim_p <- elasticnet::spca(x=dathw, K = 4, para = rep(penalties, 20),
                                type = "predictor", sparse = "penalty")
 print(spca_sim_p$pev)
 print(spca_sim_p$loadings)
 write.csv(spca_sim_p$loadings, "hw_elasticloadings.csv")
 
 ##tu ----
-spca_sim_p <- elasticnet::spca(x=dattu, K = 6, para = rep(penalties, 20),
+spca_sim_p <- elasticnet::spca(x=dattu, K = 4, para = rep(penalties, 20),
                                type = "predictor", sparse = "penalty")
 print(spca_sim_p$pev)
 print(spca_sim_p$loadings)
 write.csv(spca_sim_p$loadings, "tu_elasticloadings.csv")
 
 #correlation ----
+# Function to generate a positive definite covariance matrix
+generate_positive_definite_cov_matrix <- function(n_features, sparsity) {
+  # Generate a random sparse covariance matrix
+  cov_matrix <- matrix(0, n_features, n_features)
+  non_zero_entries <- sample(1:(n_features^2), sparsity)
+  cov_matrix[non_zero_entries] <- runif(sparsity, 0.1, 0.5)
+  cov_matrix <- Matrix::nearPD(cov_matrix)$mat  # Ensure positive definiteness
+  return(cov_matrix)
+}
+
+# Set random seed for reproducibility
+set.seed(123)
+
+# Number of samples and features
+n_samples_pca <- 515
+n_features <- 20
+sparsity <- 400  # Number of non-zero entries in the covariance matrix
+
+
+# Generate a positive definite covariance matrix with sparsity
+true_cov_matrix <- generate_positive_definite_cov_matrix(n_features, sparsity)
+
+# Generate correlated data using the covariance matrix
+background_df <- mvrnorm(n_samples, mu = rep(0, n_features), Sigma = true_cov_matrix)
+
+background_df <- data.frame(background_df)
+
+cpca_sim <- scPCA(target = datpca[, 1:20],
+                  background = background_df,
+                  penalties = .6,
+                  n_centers = 4,
+                  n_eigen = 4,
+                  scale = TRUE)
+cPC1 <- cpca_sim$x[,1]
+cPC2 <- cpca_sim$x[,2]
+
+cpca_df <- data.frame(cPC1, cPC2)
+# plot the results
+p_cpca <- ggplot(cpca_df, aes(x = cPC1, y = cPC2)) +
+  geom_point(alpha = 0.5) +
+  ggtitle("cPCA of Data") +
+  theme_minimal()
+p_cpca
+
+print(cpca_sim)
+#4, 6, 7, 9 , 17: As, Be, Cd, Co, Ag
+
+
+#correlations
 # Plot the correlation matrix of the generated data, natural log transformed
 cor_matrix <- cor(dat, use = "complete.obs")
 # Convert the correlation matrix to a long format
--- scripts_active/hds_models.R
+++ scripts_active/hds_models.R
@@ -29,12 +29,23 @@ iw.score.long <- pivot_longer(data = iw.score,
                               values_to = "concentration",
                               names_to = "analyte")
 
-#initial viz
-ggplot(iw.score.long, mapping = aes(y = log(concentration), x = score_bin, fill = score_bin)) +
+#split dataframe into different ones for each community
+iws.c <- iw.score %>%
+  group_by(community) %>%
+  group_split()
+
+iws.dh <- iws.c[[1]]
+iws.gm <- iws.c[[2]]
+iws.hw <- iws.c[[3]]
+iws.tu <- iws.c[[4]]
+
+#initial viz----
+ggplot(iw.score.long, mapping = aes(y = log(concentration), x = score_bin, fill = community)) +
   geom_boxplot() +
-  scale_fill_manual(values = wes_palette(name = "Darjeeling2", n = 5)) +
+  #scale_fill_manual(values = wes_palette(name = "Darjeeling2", n = 5)) +
+  scale_fill_manual(values=c("#F9A785", "#00A8C6", "#95CACA","#4068B2"))+
   #scale_fill_viridis_d() +
-  facet_wrap(analyte~., scales = "free") +
+  facet_grid(.~analyte, scales = "free") +
   labs(x = "\nMaintenance Score",
        y = "[analyte] (ln(ug/L))\n",
        title = "Boxplots of maintenance score by analyte",
@@ -44,47 +55,58 @@ ggplot(iw.score.long, mapping = aes(y = log(concentration), x = score_bin, fill 
         plot.title = element_text(hjust=.5, face = "bold"),
         plot.subtitle = element_text(hjust=.5),
         axis.text = element_text(vjust = .5, color = "black"),
-        axis.text.x = element_text(vjust = .5),
-        legend.position="none",
+        axis.text.x = element_text(angle = 90, vjust = .5, hjust = 1),
+        legend.position="bottom",
         panel.grid.minor = element_blank(),
         panel.grid.major.x = element_blank(),
         strip.background = element_rect(fill = "white"),
         axis.line.y = element_blank(),
         axis.line.x = element_blank())
-dev.print(png, "maintenancescore_analyte.png", res=300, height=10, width=12, units="in")
+dev.print(png, "maintenancescore_analyte_com.png", res=300, height=8, width=18, units="in")
 
-#summary ----
-##project ----
+#summaries ----
 sumFX(datalongDF = iw.score.long,
-      subset.vector.string = c("analyte", "score_bin"),
+      subset.vector.string = c("analyte", "score_bin","season", "community"),
       value.string = "concentration",
-      dfname.string = "sum.score",
-      filename.string = "score")
+      dfname.string = "sum.score.ssn.com",
+      filename.string = "score_ssncom")
 
 #zinc, aluminum, copper, iron, manganese have highest standard deviations
-#manganese, lead, copper, cadmium, aluminum, zinc, nikel have highest geometric standard deviations
+#manganese, lead, copper, cadmium, aluminum, zinc, nickel have highest geometric standard deviations
+
+#analytes with a sd > 1
+#DH
+#Al,As,Ba,Cr,Cu,Fe,Mn,Mo,Ni,Pb,Se,V,Zn
+#GM
+#Al,As,Ba,Cd,Co,Cr,Cu,Fe,Mn,Mo,Ni,Pb,Se,Sn,V,Zn
+#
+#HW
+#Al,As,Ba,Cr,Cu,Fe,Mn,Mo,Pb,Sb,Se,Sn,V,Zn
+#
+#TU
+#Al,As,Ba,Cr,Cu,Fe,Mn,Mo,Ni,Pb,Sb,Se,V,Zn
 
 #initial modeling ----
 #anovas
 
 zn.0 <- lm(data = iw.score,
-           log(Zn) ~ score_bin + community + season + proximity.km)
+           log(Zn) ~ score_bin+community + season + prox.normal)
 summary(zn.0)
 check_model(zn.0)
-performance(zn.0)
-anova(zn.0) #score signif
+performance(zn.0) #very low r2
+anova(zn.0) #score almost signif
 plot(allEffects(zn.0))
 
 al.0 <- lm(data = iw.score,
-           log(Al) ~ score_bin + community + season + proximity.km)
+           log(Al) ~ score_bin + community + season + prox.normal)
 summary(al.0)
 check_model(al.0)
-performance(al.0)
-anova(al.0) #score signif
+performance(al.0) #low r2
+anova(al.0) #score not signif
 plot(allEffects(al.0))
 
 cu.0 <- lm(data = iw.score,
-           log(Cu) ~ score_bin + community + season + proximity.km)
+           log(Cu) ~ score_bin + community + season + prox.normal)
 summary(cu.0)
 check_model(cu.0)
 performance(cu.0)
@@ -92,20 +114,95 @@ anova(cu.0) #score signif
 plot(allEffects(cu.0))
 
 pb.0 <- lm(data = iw.score,
-           log(Pb) ~ score_bin + community + season + proximity.km)
+           log(Pb) ~ score_bin + community + season + prox.normal)
 summary(pb.0)
 check_model(pb.0)
-performance(pb.0)
-anova(pb.0) #score not signif
+performance(pb.0) #low r2
+anova(pb.0) #score signif
 plot(allEffects(pb.0))
 
 cd.0 <- lm(data = iw.score,
-           log(Cd) ~ score_bin + community + season + proximity.km)
+           log(Cd) ~ score_bin + community + season + prox.normal)
 summary(cd.0)
 check_model(cd.0)
 performance(cd.0)
-anova(cd.0) #score not signif
+anova(cd.0) #score signif
 plot(allEffects(cd.0))
+
+
+mn.0 <- lm(data = iw.score,
+           log(Mn) ~ score_bin + community + season + prox.normal)
+summary(mn.0)
+check_model(mn.0)
+performance(mn.0)
+anova(mn.0) #score not signif
+plot(allEffects(mn.0))
+
+#stepwise
+#stepwise community modeling ----
+## gm ----
+
+hdslmerstepFX <- function(dataDF, analyte.string, dfname.string){
+  
+  dat <- dataDF
+  analyte <- analyte.string
+  dfname <- dfname.string
+  
+  mm.0 <- lmer(data = dat,
+               log(dat[[analyte]]) ~
+                 (1|site),
+               REML = F)
+  summary(mm.0)
+  assign(paste(dfname, ".0", sep = ""), mm.0, envir=.GlobalEnv)
+  
+  mm.1 <- lmer(data = dat,
+               log(dat[[analyte]]) ~ season + prox.normal + score_bin +
+                 (1|site),
+               REML = F)
+  summary(mm.1)
+  assign(paste(dfname, ".1", sep = ""), mm.1, envir=.GlobalEnv)
+  
+  mm.2.step <- lmerTest::step(mm.1)
+  mm.2.step
+  mm.2 <- get_model(mm.2.step)
+  print(summary(mm.2))
+  print(anova(mm.2))
+  assign(paste(dfname, ".2", sep = ""), mm.2, envir=.GlobalEnv)
+  
+}
+
+hdslmerstepFX(dataDF = iws.gm,
+              analyte.string = "Al",
+              dfname.string = "al.gm")
+
+
+mm.0 <- lmer(data = datt,
+                 log(datt[[analytee]]) ~ season +
+              (1|site),
+                 REML = F)
+datt <- iws.gm
+dfnamee <- "al.gm.0"
+analytee <- "Al"
+assign(paste(dfnamee), mm.0, envir=.GlobalEnv)
+
+#maximal.gm model
+aimn.gm.1 <- glm(data = exgm[exgm$standard=="AI"&exgm$analyte=="Mn",],
+                 exceedance ~ season +prox.normal,
+                 family = "binomial")
+summary(aimn.gm.1)
+vif(aimn.gm.1)
+check_model(aimn.gm.1)
+exp(coef(aimn.gm.1))
+performance(aimn.gm.1)
+#proximity significant
+
+aimn.gm.2 <- stepAIC(aimn.gm.0,scope = list(upper=aimn.gm.1), direction="both", trace = T)
+summary(aimn.gm.2) #prox still significant
+vif(aimn.gm.2)
+check_model(aimn.gm.2)
+exp(coef(aimn.gm.2))
+performance(aimn.gm.2)
+
 
 #functions ----
 sumFX <- function(datalongDF, subset.vector.string, value.string, dfname.string, filename.string){
