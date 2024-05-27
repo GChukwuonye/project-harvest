00b90770cdd00bd56bb9a4d94b2cb43820303ab7
Kunal Palawat <palawat.kunal@gmail.com>
2024-05-26 23:37:02 -0700

    home age

--- scripts_active/pli.R
+++ scripts_active/pli.R
@@ -29,7 +29,7 @@ library(pals)
 ##hds wrangling ----
 #each time you run the models, you need to change out the hds question you're focused on below. And apply the correct factor ordering
 iws <- iw.hds%>%
-  drop_na(Q67)%>%
+  drop_na(Q9)%>%
   mutate(Q9 = factor(Q9))%>%
   mutate(Q18 = factor(Q18))%>%
   mutate(Q44 = factor(Q44))%>%
@@ -718,13 +718,657 @@ ggplot(model.effects, aes(x = x, y = exp(predicted), ymin = exp(conf.low), ymax 
   labs(title = "When was your home built?",
        subtitle = "All Communities",
        x = "\nInfrastructure Practice",
-       y = "PLI\n")+
+       y = "Estimated PLI\n")+
   coord_cartesian(ylim = c(0,4))+
   theme_bw()+
   theme(panel.grid = element_blank(),
         legend.position = "none",
         axis.text.x = element_text(angle = 45, hjust =1))
 dev.print(png, "pli_q9_effect.png", res=300, height=6, width=10, units="in")
+
+#####Ag ----
+Ag.q9.0 <- lmer(data = iws,
+                 log(Ag) ~
+                   (1|site),
+                 REML = F)
+print(summary(Ag.q9.0))
+
+Ag.q9.1 <- lmer(data = iws,
+                 log(Ag) ~ season + prox.normal + pH + community+community:prox.normal+Q9+
+                   (1|site),
+                 REML = F)
+print(summary(Ag.q9.1))
+Ag.q9.2.step <- step(Ag.q9.1)
+Ag.q9.2.step
+Ag.q9.2 <- get_model(Ag.q9.2.step)
+print(summary(Ag.q9.2))
+check_model(Ag.q9.2)
+anova(Ag.q9.1)
+print(anova(Ag.q9.2))
+#nothing
+
+#####Al ----
+al.q9.0 <- lmer(data = iws,
+                 log(Al) ~
+                   (1|site),
+                 REML = F)
+print(summary(al.q9.0))
+
+al.q9.1 <- lmer(data = iws,
+                 log(Al) ~ season + prox.normal + pH + community+community:prox.normal+Q9+
+                   (1|site),
+                 REML = F)
+print(summary(al.q9.1))
+
+al.q9.2.step <- step(al.q9.1)
+al.q9.2.step
+al.q9.2 <- get_model(al.q9.2.step)
+print(summary(al.q9.2))
+check_model(al.q9.2)
+anova(al.q9.1)
+print(anova(al.q9.2))
+#not signif
+
+#####As ----
+As.q9.0 <- lmer(data = iws,
+                 log(As) ~
+                   (1|site),
+                 REML = F)
+print(summary(As.q9.0))
+
+As.q9.1 <- lmer(data = iws,
+                 log(As) ~ season + prox.normal + pH + community+community:prox.normal+Q9+
+                   (1|site),
+                 REML = F)
+print(summary(As.q9.1))
+
+As.q9.2.step <- step(As.q9.1)
+As.q9.2.step
+As.q9.2 <- get_model(As.q9.2.step)
+print(summary(As.q9.2))
+check_model(As.q9.2)
+vif(As.q9.2)
+anova(As.q9.1)
+print(anova(As.q9.2))
+performance(As.q9.2)
+plot(allEffects(As.q9.2))
+
+#####Ba ----
+ba.q9.0 <- lmer(data = iws,
+                 log(Ba) ~
+                   (1|site),
+                 REML = F)
+print(summary(ba.q9.0))
+
+ba.q9.1 <- lmer(data = iws,
+                 log(Ba) ~ season + prox.normal + pH + community+community:prox.normal+Q9+
+                   (1|site),
+                 REML = F)
+print(summary(ba.q9.1))
+
+ba.q9.2.step <- step(ba.q9.1)
+ba.q9.2.step
+ba.q9.2 <- get_model(ba.q9.2.step)
+print(summary(ba.q9.2))
+anova(ba.q9.1)
+print(anova(ba.q9.2))
+check_model(ba.q9.2)
+performance(ba.q9.2)
+
+ba.q9.2 <- lmer(data = iws,
+                log(Ba) ~ season + prox.normal + pH + community+community:prox.normal+Q9+
+                  (1|site),
+                REML = F)
+ba.q9.3 <- lmer(data = iws,
+                log(Ba) ~ season + prox.normal + pH + community+Q9+
+                  (1|site),
+                REML = F)
+summary(ba.q9.3)
+check_model(ba.q9.3)
+ba.q9.3.step <- step(ba.q9.3)
+ba.q9.3.step
+ba.q9.4 <- get_model(ba.q9.3.step)
+ba.q9.4 <- lmer(data = iws,
+                log(Ba) ~ season + pH + community+Q9+
+                  (1|site),
+                REML = T)
+summary(ba.q9.4)
+anova(ba.q9.4)
+check_model(ba.q9.4)
+plot(allEffects(ba.q9.4))
+ba.q9.sum <- summary(ba.q9.4)
+ba.q9.sum
+write.csv(ba.q9.sum$coefficients, "ba_q9_allcom_coefs.csv")
+perf <- performance(ba.q9.4)
+perf
+write.csv(perf, "ba_q9_allcom_diag.csv")
+#q9 signif, cleaning has higher ba
+model.effects <- ggeffect(model = ba.q9.4,
+                          type = "re",
+                          terms = c("Q9"))
+model.effects$x <- as.character(model.effects$x)
+model.effects$x  <- factor(model.effects$x , levels = c("Pre 1940", "1941-1949", "1950-1959", "1960-1969", "1970-1979", "1980-1989", "1990-1999", "2000-2009", "2010-2018"))
+ggplot(model.effects, aes(x = x, y = exp(predicted), ymin = exp(conf.low), ymax = exp(conf.high)))+
+  geom_pointrange()+
+  labs(title = "Approximately, when was your home built?",
+       subtitle = "All Communities",
+       x = "\nApproximate Home Age",
+       y = "Estimated [Ba] (ug/L)\n")+
+  #coord_cartesian(ylim = c(0,2.5))+
+  theme_bw()+
+  theme(panel.grid = element_blank(),
+        legend.position = "none",
+        axis.text.x = element_text())
+dev.print(png, "ba_q9_allcom_effect.png", res=300, height=6, width=8, units="in")
+
+
+#####Be ----
+Be.q9.0 <- lmer(data = iws,
+                 log(Be) ~
+                   (1|site),
+                 REML = F)
+print(summary(Be.q9.0))
+
+Be.q9.1 <- lmer(data = iws,
+                 log(Be) ~ season + prox.normal + pH + community+community:prox.normal+Q9+
+                   (1|site),
+                 REML = F)
+print(summary(Be.q9.1))
+Be.q9.2.step <- step(Be.q9.1)
+Be.q9.2.step
+Be.q9.2 <- get_model(Be.q9.2.step)
+print(summary(Be.q9.2))
+check_model(Be.q9.2)
+anova(Be.q9.1)
+print(anova(Be.q9.2))
+performance(Be.q9.2)
+plot(allEffects(Be.q9.2))
+#not signif
+
+Be.q9.sum <- summary(Be.q9.2)
+Be.q9.sum
+write.csv(Be.q9.sum$coefficients, "Be_q9_allcom_coefs.csv")
+perf <- performance(Be.q9.2)
+perf
+write.csv(perf, "Be_q9_allcom_diag.csv")
+#q9 signif
+model.effects <- ggeffect(model = Be.q9.2,
+                          type = "re",
+                          terms = c("Q9"))
+model.effects$x <- as.character(model.effects$x)
+model.effects$x  <- factor(model.effects$x , levels = c("Pre 1940", "1941-1949", "1950-1959", "1960-1969", "1970-1979", "1980-1989", "1990-1999", "2000-2009", "2010-2018"))
+ggplot(model.effects, aes(x = x, y = exp(predicted), ymin = exp(conf.low), ymax = exp(conf.high)))+
+  geom_pointrange()+
+  labs(title = "Approximately, when was your home built?",
+       subtitle = "All Communities",
+       x = "\nApproximate Home Age",
+       y = "Estimated [Be] (ug/L)\n")+
+  #coord_cartesian(ylim = c(0,2.5))+
+  theme_bw()+
+  theme(panel.grid = element_blank(),
+        legend.position = "none",
+        axis.text.x = element_text())
+dev.print(png, "Be_q9_allcom_effect.png", res=300, height=6, width=8, units="in")
+
+
+
+#####Cd ----
+Cd.q9.0 <- lmer(data = iws,
+                 log(Cd) ~
+                   (1|site),
+                 REML = F)
+print(summary(Cd.q9.0))
+
+Cd.q9.1 <- lmer(data = iws,
+                 log(Cd) ~ season + prox.normal + pH + community+community:prox.normal+Q9+
+                   (1|site),
+                 REML = F)
+print(summary(Cd.q9.1))
+
+Cd.q9.2.step <- step(Cd.q9.1)
+Cd.q9.2.step
+Cd.q9.2 <- get_model(Cd.q9.2.step)
+print(summary(Cd.q9.2))
+check_model(Cd.q9.2)
+
+Cd.q9.3 <- lmer(data = iws,
+                log(Cd) ~ season + prox.normal + pH + community+Q9+
+                  (1|site),
+                REML = F)
+summary(Cd.q9.3)
+check_model(Cd.q9.3)
+Cd.q9.3.step <- step(Cd.q9.3)
+Cd.q9.3.step
+Cd.q9.4 <- lmer(data = iws,
+                log(Cd) ~ season + prox.normal + community+Q9+
+                  (1|site),
+                REML = T)
+summary(Cd.q9.4)
+check_model(Cd.q9.4)
+print(anova(Cd.q9.4))
+performance(Cd.q9.4)
+plot(allEffects(Cd.q9.4))
+ba.q9.sum <- summary(ba.q9.4)
+ba.q9.sum
+write.csv(ba.q9.sum$coefficients, "ba_q9_allcom_coefs.csv")
+perf <- performance(ba.q9.4)
+perf
+write.csv(perf, "ba_q9_allcom_diag.csv")
+#q9 signif, cleaning has higher ba
+model.effects <- ggeffect(model = ba.q9.4,
+                          type = "re",
+                          terms = c("Q9"))
+model.effects$x <- as.character(model.effects$x)
+model.effects$x  <- factor(model.effects$x , levels = c("Pre 1940", "1941-1949", "1950-1959", "1960-1969", "1970-1979", "1980-1989", "1990-1999", "2000-2009", "2010-2018"))
+ggplot(model.effects, aes(x = x, y = exp(predicted), ymin = exp(conf.low), ymax = exp(conf.high)))+
+  geom_pointrange()+
+  labs(title = "Approximately, when was your home built?",
+       subtitle = "All Communities",
+       x = "\nApproximate Home Age",
+       y = "Estimated [Ba] (ug/L)\n")+
+  #coord_cartesian(ylim = c(0,2.5))+
+  theme_bw()+
+  theme(panel.grid = element_blank(),
+        legend.position = "none",
+        axis.text.x = element_text())
+dev.print(png, "ba_q9_allcom_effect.png", res=300, height=6, width=8, units="in")
+
+
+#####Co ----
+Co.q9.0 <- lmer(data = iws,
+                 log(Co) ~
+                   (1|site),
+                 REML = F)
+print(summary(Co.q9.0))
+
+Co.q9.1 <- lmer(data = iws,
+                 log(Co) ~ season + prox.normal + pH + community+community:prox.normal+Q9+
+                   (1|site),
+                 REML = F)
+print(summary(Co.q9.1))
+
+Co.q9.2.step <- step(Co.q9.1)
+Co.q9.2.step
+Co.q9.2 <- get_model(Co.q9.2.step)
+print(summary(Co.q9.2))
+check_model(Co.q9.2)
+anova(Co.q9.1)
+print(anova(Co.q9.2))
+performance(Co.q9.2)
+plot(allEffects(Co.q9.2))
+
+Co.q9.2 <- lmer(data = iws,
+                 log(Co) ~ season + prox.normal + pH + community + Q9 + (1 | site),
+                 REML = T)
+
+Co.q9.sum <- summary(Co.q9.2)
+Co.q9.sum
+write.csv(Co.q9.sum$coefficients, "Co_q9_allcom_coefs.csv")
+perf <- performance(Co.q9.2)
+perf
+write.csv(perf, "Co_q9_allcom_diag.csv")
+#q9 signif, cleaning has higher Co
+model.effects <- ggeffect(model = Co.q9.2,
+                          type = "re",
+                          terms = c("Q9"))
+model.effects$x <- as.character(model.effects$x)
+ggplot(model.effects, aes(x = x, y = exp(predicted), ymin = exp(conf.low), ymax = exp(conf.high)))+
+  geom_pointrange()+
+  labs(title = "Do you clean your roof?",
+       subtitle = "All Communities except Hayden/Winkelman",
+       x = "",
+       y = "Estimated [Co] (ug/L)\n")+
+  #coord_cartesian(ylim = c(0,2.5))+
+  theme_bw()+
+  theme(panel.grid = element_blank(),
+        legend.position = "none",
+        axis.text.x = element_text())
+dev.print(png, "Co_q9_allcom_effect.png", res=300, height=6, width=8, units="in")
+
+
+#####Cr ----
+Cr.q9.0 <- lmer(data = iws,
+                 log(Cr) ~
+                   (1|site),
+                 REML = F)
+print(summary(Cr.q9.0))
+
+Cr.q9.1 <- lmer(data = iws,
+                 log(Cr) ~ season + prox.normal + pH + community+community:prox.normal+Q9+
+                   (1|site),
+                 REML = F)
+print(summary(Cr.q9.1))
+
+Cr.q9.2.step <- step(Cr.q9.1)
+Cr.q9.2.step
+Cr.q9.2 <- get_model(Cr.q9.2.step)
+print(summary(Cr.q9.2))
+check_model(Cr.q9.2)
+anova(Cr.q9.1)
+print(anova(Cr.q9.2))
+performance(Cr.q9.2)
+
+
+Cr.q9.2 <- lmer(data = iws,
+                 log(Cr) ~ season+ Q9 + (1 | site),
+                 REML = T)
+
+Cr.q9.sum <- summary(Cr.q9.2)
+Cr.q9.sum
+write.csv(Cr.q9.sum$coefficients, "Cr_q9_allcom_coefs.csv")
+perf <- performance(Cr.q9.2)
+perf
+write.csv(perf, "Cr_q9_allcom_diag.csv")
+#q9 signif, cleaning has higher Cr
+model.effects <- ggeffect(model = Cr.q9.2,
+                          type = "re",
+                          terms = c("Q9"))
+model.effects$x <- as.character(model.effects$x)
+ggplot(model.effects, aes(x = x, y = exp(predicted), ymin = exp(conf.low), ymax = exp(conf.high)))+
+  geom_pointrange()+
+  labs(title = "Do you clean your roof?",
+       subtitle = "All Crmmunities except Hayden/Winkelman",
+       x = "",
+       y = "Estimated [Cr] (ug/L)\n")+
+  #coord_cartesian(ylim = c(0,2.5))+
+  theme_bw()+
+  theme(panel.grid = element_blank(),
+        legend.position = "none",
+        axis.text.x = element_text())
+dev.print(png, "Cr_q9_allcom_effect.png", res=300, height=6, width=8, units="in")
+
+#####Cu ----
+Cu.q9.0 <- lmer(data = iws,
+                 log(Cu) ~
+                   (1|site),
+                 REML = F)
+print(summary(Cu.q9.0))
+
+Cu.q9.1 <- lmer(data = iws,
+                 log(Cu) ~ season + prox.normal + pH + community+community:prox.normal+Q9+
+                   (1|site),
+                 REML = F)
+print(summary(Cu.q9.1))
+
+Cu.q9.2.step <- step(Cu.q9.1)
+Cu.q9.2.step
+Cu.q9.2 <- get_model(Cu.q9.2.step)
+print(summary(Cu.q9.2))
+check_model(Cu.q9.2)
+anova(Cu.q9.1)
+print(anova(Cu.q9.2))
+
+
+#####Fe ----
+Fe.q9.0 <- lmer(data = iws,
+                 log(Fe) ~
+                   (1|site),
+                 REML = F)
+print(summary(Fe.q9.0))
+
+Fe.q9.1 <- lmer(data = iws,
+                 log(Fe) ~ season + prox.normal + pH + community+community:prox.normal+Q9+
+                   (1|site),
+                 REML = F)
+print(summary(Fe.q9.1))
+
+Fe.q9.2.step <- step(Fe.q9.1)
+Fe.q9.2.step
+Fe.q9.2 <- get_model(Fe.q9.2.step)
+print(summary(Fe.q9.2))
+check_model(Fe.q9.2)
+anova(Fe.q9.1)
+print(anova(Fe.q9.2))
+performance(Fe.q9.2)
+#nothing
+
+#####Mn ----
+Mn.q9.0 <- lmer(data = iws,
+                 log(Mn) ~
+                   (1|site),
+                 REML = F)
+print(summary(Mn.q9.0))
+
+Mn.q9.1 <- lmer(data = iws,
+                 log(Mn) ~ season + prox.normal + pH + community+community:prox.normal+Q9+
+                   (1|site),
+                 REML = F)
+print(summary(Mn.q9.1))
+
+Mn.q9.2.step <- step(Mn.q9.1)
+Mn.q9.2.step
+Mn.q9.2 <- get_model(Mn.q9.2.step)
+print(summary(Mn.q9.2))
+check_model(Mn.q9.2)
+anova(Mn.q9.1)
+print(anova(Mn.q9.2))
+perf <- performance(Mn.q9.2)
+perf
+write.csv(perf, "Mntu65_diag.csv")
+plot(allEffects(Mn.q9.2))
+#season only
+
+#####Mo ----
+mo.q9.0 <- lmer(data = iws,
+                 log(Mo) ~
+                   (1|site),
+                 REML = F)
+print(summary(mo.q9.0))
+
+mo.q9.1 <- lmer(data = iws,
+                 log(Mo) ~ season + prox.normal + pH + community+community:prox.normal+Q9+
+                   (1|site),
+                 REML = F)
+print(summary(mo.q9.1))
+
+mo.q9.2.step <- step(mo.q9.1)
+mo.q9.2.step
+mo.q9.2 <- get_model(mo.q9.2.step)
+print(summary(mo.q9.2))
+anova(mo.q9.1)
+print(anova(mo.q9.2))
+check_model(mo.q9.2)
+performance(mo.q9.2)
+vif(mo.q9.2)
+mo.q9.3 <- lmer(data = iws,
+                 log(Mo) ~ season + prox.normal + pH + community+
+                   (1|site),
+                 REML = F)
+anova(mo.q9.2, mo.q9.3)
+
+mo.q9.2 <- lmer(data = iws,
+                 log(Mo) ~ season + prox.normal + pH + community + Q9 + (1 | site),
+                 REML = T)
+
+mo.q9.sum <- summary(mo.q9.2)
+mo.q9.sum
+write.csv(mo.q9.sum$coefficients, "mo_q9_allcom_coefs.csv")
+perf <- performance(mo.q9.2)
+perf
+write.csv(perf, "mo_q9_allcom_diag.csv")
+#q9 nearly signif, cleaning has higher mo
+model.effects <- ggeffect(model = mo.q9.2,
+                          type = "re",
+                          terms = c("Q9"))
+model.effects$x <- as.character(model.effects$x)
+ggplot(model.effects, aes(x = x, y = exp(predicted), ymin = exp(conf.low), ymax = exp(conf.high)))+
+  geom_pointrange()+
+  labs(title = "Do you clean your roof?",
+       subtitle = "All mommunities except Hayden/Winkelman",
+       x = "",
+       y = "Estimated [Mo] (ug/L)\n")+
+  #coord_cartesian(ylim = c(0,2.5))+
+  theme_bw()+
+  theme(panel.grid = element_blank(),
+        legend.position = "none",
+        axis.text.x = element_text())
+dev.print(png, "mo_q9_allcom_effect.png", res=300, height=6, width=8, units="in")
+
+
+#####Ni ----
+ni.q9.0 <- lmer(data = iws,
+                 log(Ni) ~
+                   (1|site),
+                 REML = F)
+print(summary(ni.q9.0))
+
+ni.q9.1 <- lmer(data = iws,
+                 log(Ni) ~ season + prox.normal + pH + community+community:prox.normal+Q9+
+                   (1|site),
+                 REML = F)
+print(summary(ni.q9.1))
+
+ni.q9.2.step <- step(ni.q9.1)
+ni.q9.2.step
+ni.q9.2 <- get_model(ni.q9.2.step)
+print(summary(ni.q9.2))
+anova(ni.q9.1)
+print(anova(ni.q9.2))
+check_model(ni.q9.2)
+vif(ni.q9.2)
+#not signif
+
+#####Pb ----
+Pb.q9.0 <- lmer(data = iws,
+                 log(Pb) ~
+                   (1|site),
+                 REML = F)
+print(summary(Pb.q9.0))
+
+Pb.q9.1 <- lmer(data = iws,
+                 log(Pb) ~ season + prox.normal + pH + community+community:prox.normal+Q9+
+                   (1|site),
+                 REML = F)
+print(summary(Pb.q9.1))
+
+Pb.q9.2.step <- step(Pb.q9.1)
+Pb.q9.2.step
+Pb.q9.2 <- get_model(Pb.q9.2.step)
+print(summary(Pb.q9.2))
+check_model(Pb.q9.2)
+anova(Pb.q9.1)
+print(anova(Pb.q9.2))
+
+#####Sb ----
+Sb.q9.0 <- lmer(data = iws,
+                 log(Sb) ~
+                   (1|site),
+                 REML = F)
+print(summary(Sb.q9.0))
+
+Sb.q9.1 <- lmer(data = iws,
+                 log(Sb) ~ season + prox.normal + pH + community+community:prox.normal+Q9+
+                   (1|site),
+                 REML = F)
+print(summary(Sb.q9.1))
+
+Sb.q9.2.step <- step(Sb.q9.1)
+Sb.q9.2.step
+Sb.q9.2 <- get_model(Sb.q9.2.step)
+print(summary(Sb.q9.2))
+check_model(Sb.q9.2)
+anova(Sb.q9.1)
+print(anova(Sb.q9.2))
+perf <- performance(Sb.q9.2)
+perf
+write.csv(perf, "Sbtu65_diag.csv")
+#nothing
+
+#####Se ----
+Se.q9.0 <- lmer(data = iws,
+                 log(Se) ~
+                   (1|site),
+                 REML = F)
+print(summary(Se.q9.0))
+
+Se.q9.1 <- lmer(data = iws,
+                 log(Se) ~ season + prox.normal + pH + community+community:prox.normal+Q9+
+                   (1|site),
+                 REML = F)
+print(summary(Se.q9.1))
+
+Se.q9.2.step <- step(Se.q9.1)
+Se.q9.2.step
+Se.q9.2 <- get_model(Se.q9.2.step)
+print(summary(Se.q9.2))
+check_model(Se.q9.2)
+anova(Se.q9.1)
+print(anova(Se.q9.2))
+performance(Se.q9.2)
+#season only
+
+#####Sn ----
+Sn.q9.0 <- lmer(data = iws,
+                 log(Sn) ~
+                   (1|site),
+                 REML = F)
+print(summary(Sn.q9.0))
+
+Sn.q9.1 <- lmer(data = iws,
+                 log(Sn) ~ season + prox.normal + pH + community+community:prox.normal+Q9+
+                   (1|site),
+                 REML = F)
+print(summary(Sn.q9.1))
+
+Sn.q9.2.step <- step(Sn.q9.1)
+Sn.q9.2.step
+Sn.q9.2 <- get_model(Sn.q9.2.step)
+print(summary(Sn.q9.2))
+check_model(Sn.q9.2)
+anova(Sn.q9.1)
+print(anova(Sn.q9.2))
+performance(Sn.q9.2)
+#nothing
+
+#####V ----
+v.q9.0 <- lmer(data = iws,
+                log(V) ~
+                  (1|site),
+                REML = F)
+print(summary(v.q9.0))
+
+v.q9.1 <- lmer(data = iws,
+                log(V) ~ season + prox.normal + pH + community+community:prox.normal+Q9+
+                  (1|site),
+                REML = F)
+print(summary(v.q9.1))
+
+v.q9.2.step <- step(v.q9.1)
+v.q9.2.step
+v.q9.2 <- get_model(v.q9.2.step)
+print(summary(v.q9.2))
+anova(v.q9.1)
+print(anova(v.q9.2))
+check_model(v.q9.2)
+vif(v.q9.2)
+performance(v.q9.2)
+
+#not signif
+
+
+#####Zn ----
+Zn.q9.0 <- lmer(data = iws,
+                 log(Zn) ~
+                   (1|site),
+                 REML = F)
+print(summary(Zn.q9.0))
+
+Zn.q9.1 <- lmer(data = iws,
+                 log(Zn) ~ season + prox.normal + pH + community+community:prox.normal+Q9+
+                   (1|site),
+                 REML = F)
+print(summary(Zn.q9.1))
+
+Zn.q9.2.step <- step(Zn.q9.1)
+Zn.q9.2.step
+Zn.q9.2 <- get_model(Zn.q9.2.step)
+print(summary(Zn.q9.2))
+check_model(Zn.q9.2)
+anova(Zn.q9.1)
+print(anova(Zn.q9.2))
+performance(Zn.q9.2)
+#nothing
+
 
 ###dewey-humboldt ----
 pli.q9.dh.0 <- lmer(data = iws.dh,
@@ -1203,6 +1847,8 @@ plot(allEffects(pli.q1.3))
 #         legend.position = "none",
 #         axis.text.x = element_text(angle = 45, hjust = 1))
 # #dev.print(png, "Ni_tu_q67_effect.png", res=300, height=8, width=10, units="in")
+
+
 
 ###globe/miami ----
 pli.q1.gm.0 <- lmer(data = iws.gm,
